616b1eb918bf37d1b1f1aadd7714ccdc36535d7c
Enzo PB <enzo.puiroux@gmail.com>
2024-04-24 10:03:36 +0200

    handle encode when data_part is a string

--- minecraft/types/__init__.py
+++ minecraft/types/__init__.py
@@ -38,13 +38,25 @@ types_names = {
 
 
 def encode_field(data: dict, data_part: dict | list) -> bytearray:  # automatically decode a field
+    # eg. data_part = ['container', [{'type': 'i64', 'name': 'field'}, ...]]
     if isinstance(data_part, list):  # if datapart is a list, it is a container (or an array? or something else)
         if data_part[0] == 'container':  # a container is just a list of different items (usually contained inside a condition)
             buffer = bytearray()
             for item in data_part[1]:
                 buffer.extend(encode_field(data, item))
             return buffer
-    else:
+
+    # eg. data_part = 'UUID'
+    if isinstance(data_part, str):
+        if data_part in types_names:
+            var_type: MCNativeType = types_names[data_part]  # get the corresponding type class
+        else:
+            raise UnknownType(f'Tried to encode unknown type: {data_part}')
+
+        return var_type.encode(data)
+
+    # eg. data_part = {'type': 'i64', 'name': 'field'}
+    elif isinstance(data_part, dict):
         if isinstance(data_part['type'], str):  # type can either be a string (native type)
             if data_part['type'] in types_names:
                 var_type: MCNativeType = types_names[data_part['type']]  # get the corresponding type class
