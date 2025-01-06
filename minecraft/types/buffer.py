ac963a077220a0b00e70599010839b06dfadbe26
Enzo PB <enzo.puiroux@mail.com>
2025-01-06 14:25:12 +0100

    feat: buffer decode

--- minecraft/types/buffer.py
+++ minecraft/types/buffer.py
@@ -1,12 +1,19 @@
 from .mc_special_type import MCSpecialType
-from .boolean import Boolean
+import io
 
 
 class Buffer(MCSpecialType):
     @staticmethod
     def encode(data: bytearray, data_part: list) -> bytearray:
         buffer = bytearray()
-        from . import types_names, encode_field  # import this here (not at top) to avoid circular import loop
+        from . import types_names  # import this here (not at top) to avoid circular import loop
         buffer.extend(types_names[data_part[1]['countType']].encode(len(data)))  # we add the length of the buffer, whose type can vary
         buffer.extend(data)
         return buffer
+
+    @staticmethod
+    def decode(stream: io.IOBase, structure: any) -> bytearray:
+        from . import types_names
+        length = types_names[structure['countType']].decode(stream)
+        data = stream.read(length)
+        return bytearray(data)
